f83cbc140e9dcccf08de33e0439d0beecec56861
Scouttman <sctt.frsr@gmail.com>
2015-10-04 14:54:29 +1100

    blind hunter

--- hunter.c
+++ hunter.c
@@ -6,9 +6,34 @@
 #include "Game.h"
 #include "HunterView.h"
 
+
+
 void decideHunterMove(HunterView gameState)
 {
-    // TODO ...
-    // Replace the line below by something better
-    registerBestPlay("GE","I'm on holiday in Geneva");
+   int round = giveMeTheRound(gameState);
+   LocationID trail[TRAIL_SIZE];
+   giveMeTheTrail(gameState, whoAmI(gameState), trail);
+   int dead = 0;
+   int i;
+   for(i=4; i<6; i++){
+      if(trail[i] == nameToID("JM"))
+         dead = i;
+   }
+   
+   if(dead){
+      if(round%2==0){
+         registerBestPlay("KL","Dead 1");
+      }else if(round%2==1){
+         registerBestPlay("GA","Dead 2");
+      }
+   }else{
+      if(dead == 4){
+         registerBestPlay("SZ","");
+      }else if(dead == 5){
+         registerBestPlay("KL","");
+      }else{
+         registerBestPlay("KL","");
+      }
+   } 
+   registerBestPlay("GE","I'm on holiday in Geneva");
 }
