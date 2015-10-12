218167970d9edf2efec14eb6bd9d21efff64e6d0
azeng97 <azeng97@gmail.com>
2015-10-12 15:21:54 +1100

    Update Places.h
    added idToAbbrev

--- Places.h
+++ Places.h
@@ -129,6 +129,8 @@ int nameToID(char *name);
 // given a Place abbreviation, return its ID number
 int abbrevToID(char *abbrev);
 
+char *idToAbbrev(int place);
+
 #define isLand(place)  (idToType(place) == LAND)
 #define isSea(place)  (idToType(place) == SEA)
 
